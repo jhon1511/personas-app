64d79bef3416b3b2873350e0591f52b115681338
Usuario1 <Usuario1@DESKTOP-LAS9AIG>
2024-03-29 17:08:43 -0500

    delete municipioí

--- resources/views/Municipio/index.blade.php
+++ resources/views/Municipio/index.blade.php
@@ -28,7 +28,16 @@
                 <th scope="row">{{$municipio->muni_codi}}</th>
                 <td>{{$municipio->muni_nomb}}</td>
                 <td>{{$municipio->depa_nomb}}</td>
-                <td>actions</td>
+                <td>
+
+                <form action="{{ route('municipios.destroy', ['municipio' => $municipio->muni_codi]) }}"
+                method='POST' style="display: inline-block">
+                @method('delete')
+                @csrf
+                <input class="btn btn-danger" type="submit" value="Delete">
+                </form>
+
+                </td>
                 
                 </tr>
                 @endforeach
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/comunas/{comuna}/edit',[ComunaController::class,'edit'])->name('com
 Route::get('/municipios',[MunicipioController::class,'index'])->name('municipios.index');
 Route::post('/municipios',[MunicipioController::class,'store'])->name('municipios.store');
 Route::get('/municipios/create',[MunicipioController::class,'create'])->name('municipios.create');
+Route::delete('/municipios/{municipio}',[MunicipioController::class,'destroy'])->name('municipios.destroy');
+
