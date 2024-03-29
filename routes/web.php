4cffaea3841ed4e5235c374edf0a517e2ef64aa1
Usuario1 <Usuario1@DESKTOP-LAS9AIG>
2024-03-29 15:48:46 -0500

    index municipioí

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/comunas',[ComunaController::class,'index'])->name('comunas.index');
+Route::post('/comunas',[ComunaController::class,'store'])->name('comunas.store');
+Route::get('/comunas/create',[ComunaController::class,'create'])->name('comunas.create');
+Route::delete('/comunas/{comuna}',[ComunaController::class,'destroy'])->name('comunas.destroy');
+Route::put('/comunas/{comuna}',[ComunaController::class,'update'])->name('comunas.update');
+Route::get('/comunas/{comuna}/edit',[ComunaController::class,'edit'])->name('comunas.edit');
+//Ruta MUNICIPIO
+Route::get('/municipios',[MunicipioController::class,'index'])->name('municipios.index');
+
