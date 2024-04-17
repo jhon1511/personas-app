d8d369dcf1e856c159a85c28db0df1763a095141
unknown <stevenramirez1115@gmail.com>
2024-04-17 18:38:32 -0500

    Modificado de diseno

--- resources/views/Municipio/new.blade.php
+++ resources/views/Municipio/new.blade.php
@@ -1,55 +1,47 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-app-layout>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        
-        <title>Add Municipality</title>
-  </head>
+<!-- JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ __('Add Municipio') }}</h2>
+    </x-slot>
 
-  <body>
-    <div class="container">
-    <h1>Add Municipality</h1>
-    <form method="POST" action="{{ route ('municipios.store') }}">
-    @csrf
-    <div class="mb-3">
-    <label for="id" class="form-label">Code</label>
-    <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" >
-    <div id="idHelp" class="form-text">Pais Code</div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="container">
+                        <h1>Add Municipio</h1>
+                        <form method="POST" action="{{route('municipios.store')}}">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="id" class="form-label">Code</label>
+                                <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled">
+                                <div id="idHelp" class="form-text">Municipio Code</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Municipio</label>
+                                <input type="text" class="form-control" id="name" aria-describedby="nameHelp" name="name" placeholder="Municipio name.">
+                            </div>
+                            <label for="departamento">Departamento</label>
+                            <select class="form-select" id="departamento" name="code" required>
+                                <option selected disabled value="">Elige una...</option>
+                                @foreach($departamentos as $departamento)
+                                    <option value="{{$departamento->depa_codi}}">{{$departamento->depa_nomb}}</option>
+                                @endforeach
+                            </select>
+                            <div class="mt-3">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                                <a href="{{route('municipios.index')}}" class="btn btn-warning">Cancel</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="mb-3">
-    <label for="name" class="form-label">Municipality</label>
-    <input type="text" required class="form-control" id="name" aria-describedby="nameHelp"
-    name="name" placeholder="Departamento name.">
-    </div>
-    <label for="departamento">Departamento:</label>
-    <select class="form-select" id="departamento" name="code" required>
-        <option selected disabled value="">Choose one...</option>
-        @foreach ($departamentos as $departamento)
-        <option value="{{ $departamento->depa_codi }}"> {{ $departamento->depa_nomb }} </option>
-        @endforeach
-</select>
-    <div class="mt-3">
-    <button type="submit" class="btn btn-primary">Save</button>
-    <a href="{{ route('municipios.index') }}" class="btn btn-warning">Cancel</a>
-    </div>
-</form>
-</div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="/path/to/bootstrap.bundle.min.js"></script>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+</x-app-layout>
--- resources/views/Pais/new.blade.php
+++ resources/views/Pais/new.blade.php
@@ -1,58 +1,48 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-app-layout>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        
-        <title>Add Pais</title>
-  </head>
+<!-- JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-  <body>
-    <div class="container">
-    <h1>Add Pais</h1>
-    <a href="{{ route('paises.create')}}" class= "btn btn-success">Add</a>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ __('Add Pais') }}</h2>
+    </x-slot>
 
-    <form method="POST" action="{{ route ('paises.store') }}">
-    @csrf
-    <div class="mb-3">
-    <label for="id" class="form-label">Code</label>
-    <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id"
-        >
-    <div id="idHelp" class="form-text">Pais code</div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="container">
+                        <h1>Add Pais</h1>
+                        <form method="POST" action="{{route('paises.store')}}">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="id" class="form-label">Code</label>
+                                <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id">
+                                <div id="idHelp" class="form-text">Pais Code</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Pais</label>
+                                <input type="text" required class="form-control" id="name" aria-describedby="nameHelp" name="name" placeholder="Pais Name.">
+                            </div>
+                            <label for="municipality">Capital:</label>
+                            <select class="form-select" name="code" id="municipality" required>
+                                <option selected disabled value="">Choose one...</option>
+                                @foreach ($municipios as $municipio)
+                                    <option value="{{$municipio->muni_codi}}">{{$municipio->muni_nomb}}</option>
+                                @endforeach
+                            </select>
+                            <div class="mt-3">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                                <a href="{{route('paises.index')}}" class="btn btn-warning">Cancel</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="mb-3">
-    <label for="name" class="form-label">Pais</label>
-    <input type="text" required class="form-control" id="name" aria-describedby="nameHelp"
-    name="name" placeholder="Municipio name.">
-    </div>
-    <label for="municipio">Municipio:</label>
-    <select class="form-select" id="pais" name="code" required>
-        <option selected disabled value="">Choose one...</option>
-        @foreach ($municipios as $municipio)
-        <option value="{{ $municipio->muni_codi }}"> {{ $municipio->muni_nomb }} </option>
-        @endforeach
-</select>
-    <div class="mt-3">
-    <button type="submit" class="btn btn-primary">Save</button>
-    <a href="{{ route('municipios.index') }}" class="btn btn-warning">Cancel</a>
-    </div>
-</form>
-</div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="/path/to/bootstrap.bundle.min.js"></script>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+</x-app-layout>
